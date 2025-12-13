f10fa9881f1af7aa25c564b24d3e29e13b0283f5
Arnes Abel <arnesabel@gmail.com>
2025-12-13 16:20:56 -0400

    Fix

--- Chatbot.Core/Entities/User.cs
+++ Chatbot.Core/Entities/User.cs
@@ -2,7 +2,7 @@
 
 namespace Chatbot.Core.Entities
 {
-    internal class User : IdentityUser<int>
+    public class User : IdentityUser<int>
     {
         public required string DisplayName { get; set; }
         public ICollection<ChatMessage> Messages { get; set; } = new HashSet<ChatMessage>();
